5f0ea11ff9c1930d97054ddfd2096113df113668
ASManikantha <asmanikanta04@gmail.com>
2024-09-15 23:30:51 +0530

    Update simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,46 +1,118 @@
-import random
-import math
+from datetime import datetime
+import random, time, math
+from copy import deepcopy, copy
+import decimal
 
-def generate_initial_state():
-    return list(range(8))
+class Board:
+    def __init__(self, queen_count=8):
+        self.queen_count = queen_count
+        self.reset()
 
-def cost_function(state):
-    attacks = 0
-    for i in range(len(state)):
-        for j in range(i + 1, len(state)):
-            if state[i] == state[j] or abs(state[i] - state[j]) == j - i:
-                attacks += 1
-    return attacks
+    def reset(self):
+        self.queens = [-1 for i in range(0, self.queen_count)]
 
-def get_neighbors(state):
-    neighbors = []
-    for i in range(len(state)):
-        for j in range(i + 1, len(state)):
-            neighbor = state[:]
-            neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
-            neighbors.append(neighbor)
-    return neighbors
+        for i in range(0, self.queen_count):
+            self.queens[i] = random.randint(0, self.queen_count - 1)
+            # self.queens[row] = column
 
-def simulated_annealing():
-    current_state = generate_initial_state()
-    current_cost = cost_function(current_state)
-    temperature = 1.0
-    cooling_rate = 0.99
 
-    while temperature > 0.01:
-        neighbors = get_neighbors(current_state)
-        next_state = random.choice(neighbors)
-        next_cost = cost_function(next_state)
+    def calculateCost(self):
+        threat = 0
 
-        if next_cost < current_cost or random.uniform(0, 1) < math.exp((current_cost - next_cost) / temperature):
-            current_state = next_state
-            current_cost = next_cost
+        for queen in range(0, self.queen_count):
+            for next_queen in range(queen+1, self.queen_count):
+                if self.queens[queen] == self.queens[next_queen] or abs(queen - next_queen) == abs(self.queens[queen] - self.queens[next_queen]):
+                    threat += 1
 
-        temperature *= cooling_rate
+        return threat
 
-    return current_state, current_cost
+    @staticmethod
+    def calculateCostWithQueens(queens):
+        threat = 0
+        queen_count = len(queens)
 
-if __name__ == "__main__":
-    sa_solution, sa_cost = simulated_annealing()
-    print("SA Solution:", sa_solution)
-    print("SA Cost:", sa_cost)
+        for queen in range(0, queen_count):
+            for next_queen in range(queen+1, queen_count):
+                if queens[queen] == queens[next_queen] or abs(queen - next_queen) == abs(queens[queen] - queens[next_queen]):
+                    threat += 1
+
+        return threat
+
+    @staticmethod
+    def toString(queens):
+        board_string = ""
+
+        for row, col in enumerate(queens):
+            board_string += "(%s, %s)\n" % (row, col)
+
+        return board_string
+
+    def getLowerCostBoard(self):
+        displacement_count = 0
+        temp_queens = self.queens
+        lowest_cost = self.calculateCost(temp_queens)
+
+        for i in range(0, self.queen_count):
+            temp_queens[i] = (temp_queens[i] + 1) % (self.queen_count - 1)
+
+            for j in range(queen+1, self.queen_count):
+                temp_queens[j] = (temp_queens[j] + 1) % (self.queen_count - 1)
+
+    def __str__(self):
+        board_string = ""
+
+        for row, col in enumerate(self.queens):
+            board_string += "(%s, %s)\n" % (row, col)
+
+        return board_string
+
+class SimulatedAnnealing:
+    def __init__(self, board):
+        self.elapsedTime = 0;
+        self.board = board
+        self.temperature = 4000
+        self.sch = 0.99
+        self.startTime = datetime.now()
+
+
+    def run(self):
+        board = self.board
+        board_queens = self.board.queens[:]
+        solutionFound = False
+
+        for k in range(0, 170000):
+            self.temperature *= self.sch
+            board.reset()
+            successor_queens = board.queens[:]
+            dw = Board.calculateCostWithQueens(successor_queens) - Board.calculateCostWithQueens(board_queens)
+            exp = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(-dw) * decimal.Decimal(self.temperature)))
+
+            if dw > 0 or random.uniform(0, 1) < exp:
+                board_queens = successor_queens[:]
+
+            if Board.calculateCostWithQueens(board_queens) == 0:
+                print("Solution:")
+                print(Board.toString(board_queens))
+                self.elapsedTime = self.getElapsedTime()
+                print("Success, Elapsed Time: %sms" % (str(self.elapsedTime)))
+                solutionFound = True
+                break
+
+        if solutionFound == False:
+            self.elapsedTime = self.getElapsedTime()
+            print("Unsuccessful, Elapsed Time: %sms" % (str(self.elapsedTime)))
+
+        return self.elapsedTime
+
+    def getElapsedTime(self):
+        endTime = datetime.now()
+        elapsedTime = (endTime - self.startTime).microseconds / 1000
+        return elapsedTime
+
+
+if __name__ == '__main__':
+    board = Board()
+    print("Board:")
+    print(board)
+    SimulatedAnnealing(board).run()
+
